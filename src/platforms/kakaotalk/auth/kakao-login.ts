4830e05026359ead676979dbafbd573eeafb1fea
Jeon Suyeol <devxoul@gmail.com>
2026-03-21 00:03:55 +0900

    Use tablet model name (SM-T870) for tablet device slot login

--- src/platforms/kakaotalk/auth/kakao-login.ts
+++ src/platforms/kakaotalk/auth/kakao-login.ts
@@ -14,7 +14,7 @@ const ANDROID_LOGIN_URL = 'https://katalk.kakao.com/android/account/login.json'
 const ANDROID_PASSCODE_URL = 'https://katalk.kakao.com/android/account/passcodeLogin/generate'
 const ANDROID_REGISTER_URL = 'https://katalk.kakao.com/android/account/passcodeLogin/registerDevice'
 
-const DEVICE_NAME = 'SM-G998N'
+const DEVICE_NAME = 'SM-T870'
 const DEVICE_UUID_LENGTH = 64
 
 function generateDeviceUuid(): string {
