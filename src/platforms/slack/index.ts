77520e4c5ec677514f208667f6ea854db426a909
Jeon Suyeol <devxoul@gmail.com>
2026-03-20 18:45:13 +0900

    Export all types and Zod schemas from Slack barrel

--- src/platforms/slack/index.ts
+++ src/platforms/slack/index.ts
@@ -1,2 +1,28 @@
 export { SlackClient, SlackError } from './client'
 export { CredentialManager } from './credential-manager'
+export type {
+  SlackChannel,
+  SlackMessage,
+  SlackUser,
+  SlackReaction,
+  SlackFile,
+  SlackSearchResult,
+  SlackUnreadCounts,
+  SlackThreadView,
+  SlackSavedItem,
+  SlackActivityItem,
+  SlackDM,
+  SlackDraft,
+  SlackChannelSection,
+  WorkspaceCredentials,
+  Config,
+} from './types'
+export {
+  SlackChannelSchema,
+  SlackReactionSchema,
+  SlackFileSchema,
+  SlackMessageSchema,
+  SlackUserSchema,
+  WorkspaceCredentialsSchema,
+  ConfigSchema,
+} from './types'
