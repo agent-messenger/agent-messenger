25f64140e31d03a7ac4a61062852661242ae9bb1
Jeon Suyeol <devxoul@gmail.com>
2026-03-23 22:10:23 +0900

    Add createKakaoTalkClient() for automatic credential extraction

--- src/platforms/kakaotalk/index.ts
+++ src/platforms/kakaotalk/index.ts
@@ -16,3 +16,12 @@ export {
   KakaoMessageSchema,
   KakaoSendResultSchema,
 } from './types'
+
+import { KakaoTalkClient, KakaoTalkError } from './client'
+import { CredentialManager } from './credential-manager'
+
+export async function createKakaoTalkClient(): Promise<KakaoTalkClient> {
+  const { ensureKakaoAuth } = await import('./ensure-auth')
+  const account = await ensureKakaoAuth()
+  return new KakaoTalkClient(account.oauth_token, account.user_id, account.device_uuid)
+}
