de48120942e48771ffd1c79b8340be485c60c982
Jeon Suyeol <devxoul@gmail.com>
2026-03-25 11:14:40 +0900

    Refactor WhatsAppBotClient to use zero-param constructor and login() method

--- src/platforms/whatsappbot/index.ts
+++ src/platforms/whatsappbot/index.ts
@@ -15,18 +15,4 @@ export {
   WhatsAppBotError,
 } from './types'
 
-import { WhatsAppBotClient } from './client'
-import { WhatsAppBotCredentialManager } from './credential-manager'
-import { WhatsAppBotError } from './types'
 
-export async function createWhatsAppBotClient(): Promise<WhatsAppBotClient> {
-  const credManager = new WhatsAppBotCredentialManager()
-  const creds = await credManager.getCredentials()
-  if (!creds) {
-    throw new WhatsAppBotError(
-      'No WhatsApp Bot credentials found. Run "agent-whatsappbot auth set" first.',
-      'no_credentials',
-    )
-  }
-  return new WhatsAppBotClient(creds.phone_number_id, creds.access_token)
-}
