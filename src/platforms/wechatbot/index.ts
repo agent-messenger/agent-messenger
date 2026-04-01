7ac63f820b13c7ccbcb085111626ebea52b71da2
Jeon Suyeol <devxoul@gmail.com>
2026-04-01 17:26:30 +0900

    Add WeChat bot CLI entry point and barrel export

---
+++ src/platforms/wechatbot/index.ts
@@ -0,0 +1,19 @@
+export { WeChatBotClient } from './client'
+export { WeChatBotCredentialManager } from './credential-manager'
+export type {
+  WeChatBotAccountEntry,
+  WeChatBotConfig,
+  WeChatBotCredentials,
+  WeChatBotNewsArticle,
+  WeChatBotTemplate,
+  WeChatBotUserInfo,
+} from './types'
+export {
+  WeChatBotAccountEntrySchema,
+  WeChatBotConfigSchema,
+  WeChatBotCredentialsSchema,
+  WeChatBotError,
+  WeChatBotNewsArticleSchema,
+  WeChatBotTemplateSchema,
+  WeChatBotUserInfoSchema,
+} from './types'
